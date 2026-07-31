dd944110d36514a6ee73569f87d96d12369ca16a
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-07-31 10:48:19 +0600

    explore about function

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-"use strict";
+// "use strict";
 
-let hasDriversLincense = false;
-const passTest = true;
+// let hasDriversLincense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLincense = true;
-if (hasDriversLincense) console.log("I can Drive");
+// if (passTest) hasDriversLincense = true;
+// if (hasDriversLincense) console.log("I can Drive");
 
 // const interface = 'audio';
 // const private = 245;
-const if = 3234;
+// const if = 3234;
+
+//function
+function logger() {
+  console.log("My name is Mostafijur");
+}
+
+// logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `juice with ${apples} apple ane ${oranges} oranges.`;
+  return juice;
+}
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
