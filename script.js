e70550dc5558765e341a113076e7f377a537e521
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-07-31 10:15:39 +0600

    explore about strict mode

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let hasDriversLincense = false;
+const passTest = true;
+
+if (passTest) hasDriversLincense = true;
+if (hasDriversLincense) console.log("I can Drive");
+
+// const interface = 'audio';
+// const private = 245;
+const if = 3234;
